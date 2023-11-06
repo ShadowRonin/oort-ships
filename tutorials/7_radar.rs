c5f2c191847d1b7ed7532b484ad59a1894a9fa5b
ShadowRonin <shadowronin42@gmail.com>
2023-11-06 12:59:07 -0500

    improve radar code

--- tutorials/7_radar.rs
+++ tutorials/7_radar.rs
@@ -7,227 +7,181 @@
 //
 // Join the Discord at https://discord.gg/vYyu9EhkKH for Oort discussion and
 // tournament results.
+
 use oort_api::prelude::*;
-
-const BULLET_SPEED: f64 = 1000.0; // m/s
-
-struct Target {
-    last_seen: f64,
-    hits: Vec<ScanHit>,
-    expire_after: f64, // Number of seconds before this target is no longer valid
-}
-
-impl Target {
-    fn new(sr: ScanResult) -> Target {
-        Target {
-            last_seen: current_time(),
-            hits: vec!(ScanHit::new(sr)),
-            expire_after: 0.5,
-        }
-    }
-
-    fn match_last_seen(&self, scan: &ScanResult) -> bool {
-        let max_aceleration = 2.0 * max_forward_acceleration();
-
-        let last_scan = &self.hits.last().unwrap();
-        let dt = current_time() - last_scan.time;
-        
-        let a = (vec2(scan.velocity.x, scan.velocity.y) - last_scan.result.velocity) / dt;
-        if a.length() > max_aceleration { return false }
-
-        let predicted_position = self.future_position(current_time(), true);
-        let dp = predicted_position - scan.position;
-        if dp.length() > (max_aceleration * dt) { return false }
-
-        true
-    }
-
-    fn future_position(&self, t: f64, ignore_aceleration: bool) -> Vec2 {
-        let scan1 = &self.hits.last().unwrap();
-        let scan0 = &self.hits.get(self.hits.len() - 2);
-        
-        // Hint: p = p₀ + v₀t + ½at² (the third equation of kinematics)
-        let p0 = scan1.result.position;
-        let v = scan1.result.velocity;
-        let a = 
-            if ignore_aceleration || scan0.is_none() {vec2(0.0,0.0)} 
-            else { scan1.aceleration(scan0.unwrap()) };
-        let dt = t - scan1.time;
-
-        p0 + (v * dt) + (0.5 * a * (dt * dt))
-    }
-
-    fn add_scan(&mut self, sr: ScanResult) {
-        self.last_seen = current_time();
-        self.hits.push(ScanHit::new(sr));
-    }
-
-    fn position(&self) -> Vec2 {
-        let scan1 = &self.hits.last().unwrap();
-        vec2(scan1.result.position.x, scan1.result.position.y)
-    }
-
-    fn velocity(&self) -> Vec2 {
-        let scan1 = &self.hits.last().unwrap();
-        vec2(scan1.result.velocity.x, scan1.result.velocity.y)
-    }
-
-    fn aceleration(&self) -> Vec2 {
-        let scan1 = &self.hits.last().unwrap();
-        let scan0 = &self.hits.get(self.hits.len() - 2);
-
-        if let Some(scan0) = scan0 {
-            scan1.aceleration(scan0)
-        } else {
-            vec2(0.0, 0.0)
-        }
-    }
-
-    fn has_expired(&self) -> bool {
-        current_time() - self.last_seen >= self.expire_after
-    }
-
-}
-
-//struct ScanResult { position: Vec2, velocity: Vec2 }
-struct ScanHit {
-    result: ScanResult,
-    time: f64,
-}
-impl ScanHit {
-    fn new(result: ScanResult) -> ScanHit {
-        ScanHit {
-            time: current_time(),
-            result
-        }
-    }
-
-    fn aceleration(&self, hit: &ScanHit) -> Vec2 {
-        let dt = self.time - hit.time;
-        (self.result.velocity - hit.result.velocity) / dt
-    }
-}
 
 fn degree_to_radian(deg: f64) -> f64 {
     deg * (PI / 180.0)
 }
+fn radian_to_degree(r: f64) -> f64 {
+    r * 180.0 / PI
+}
+
+const BULLET_SPEED: f64 = 1000.0; // m/s
 
 pub struct Ship {
-    closest_target: Option<Target>,
-    search: bool,
-    search_start: f64,
+    scan_result: Option<ScanResult>,
+    prev_scan_result: Option<ScanResult>,
+
+    seconds_before_using_turn: f64,
+    firing_offset: f64,
+
+    debug_turn: bool,
 }
 
 impl Ship {
     pub fn new() -> Ship {
         Ship {
-            closest_target: None,
-            search: false,
-            search_start: 0.0,
+            scan_result: Option::None,
+            prev_scan_result: Option::None,
+
+            seconds_before_using_turn: TICK_LENGTH,
+            firing_offset: degree_to_radian(0.05),
+
+
+            debug_turn: false,
         }
+    }
+
+    fn calculate_p1(&mut self) -> Vec2 {
+        let target = self.scan_result.as_ref().unwrap();
+        let p0 = target.position;
+        let v = target.velocity;
+        let d = p0 - position(); // distance to target
+        let t = d.length() / BULLET_SPEED; // how long before our bullets reach the target
+        
+        // calculate the acceleration. 
+        // Note: TICK_LENGTH is the amount of seconds of a single tick
+        // Since we are checking 'v' every tick, then this is the amount of time since the last time we updated 'v'
+        let a: Vec2;
+        if self.prev_scan_result.is_some() {
+            let prev_v = self.prev_scan_result.as_ref().unwrap().velocity;
+            a = (v - prev_v) / TICK_LENGTH;
+        } else {
+            a = vec2(0.0, 0.0);
+        }
+
+        // note that we now account for 'a'
+        let mut p1 = p0 + v * t + 0.5 * a * t.powi(2);
+        
+        for _ in 0..100 {
+            let d = p1 - position();
+            let t = d.length() / BULLET_SPEED;
+            p1 = p0 + v * t  + 0.5 * a * t.powi(2);
+        }
+
+        p1
     }
 
     fn scan(&mut self) {
-        // Process scan
-        if let Some(s) = scan() {
-            if self.closest_target.is_some() {
-                let t = self.closest_target.as_mut().unwrap();
-                if t.match_last_seen(&s) {
-                    t.add_scan(s);
-                } else if s.position.distance(position()) < t.position().distance(position()) {
-                    self.closest_target = Some(Target::new(s));
-                }
-            } else {
-                self.closest_target = Some(Target::new(s));
-            }
-        }
+        // Attempt to get info from our radar
+        if let Some(scan) = scan() {
+            // Turns the radar towards the target
+            let towards_scan  = scan.position - position();
+            set_radar_heading(towards_scan.angle());
 
-        // Remove expired target
-        if self.closest_target.as_ref().is_some() && self.closest_target.as_ref().unwrap().has_expired() {
-            self.closest_target = None;
-        }
-
-        // Move rader
-        if self.search {
-            if radar_heading() < degree_to_radian(10.0) && self.closest_target.is_some() {
-                self.search = false;
-            } else {
-                self.search_start = self.search_start + degree_to_radian(10.0);
-                set_radar_heading(self.search_start);
-            }
-        } else if self.closest_target.is_some() {
-            let fp = self.closest_target.as_ref()
-                .unwrap()
-                .future_position(current_time() + TICK_LENGTH, false);
-
-            set_radar_heading((fp - position()).angle());
+            // Update the scan results
+            self.prev_scan_result = std::mem::replace(&mut self.scan_result, Some(scan));
         } else {
-            self.search = true;
-            self.search_start = 10.0;
-            set_radar_heading(self.search_start);
+            // Turns the radar in a circle until we find a target
+            set_radar_heading(radar_heading() + radar_width());
+
+            // Remove old scans, as we have lost, or destoryed, the target
+            self.prev_scan_result = Option::None;
+            self.scan_result = Option::None;
         }
     }
 
-    fn calc_future_target(&self, depth_of_calc: u32) -> Vec2 {
-        // Hint: p = p₀ + v₀t + ½at² (the third equation of kinematics)
-        let target = self.closest_target.as_ref().unwrap();
-        
-        let p0 = target.position();
-        let v = target.velocity();
-        let mut t = (target.position() - position()).length() / BULLET_SPEED;
-        let a = target.aceleration();
-        let mut pf = p0 + (v * t) + (0.5 * a * (t * t));
+    // Use torque to turn faster
+    fn turn(&self, target_heading: f64) {
+        // While `turn` turns you to a given heading
+        // `torque` accelerates you in a spin
+        // We can calculate the optimal amount to `torque`
+        // Allowing us to turn much faster
 
-        // TODO: maybe just have some percent offset for t, instead of recalculating
-        for _ in [..depth_of_calc] {
-            t = (pf - position()).length() / BULLET_SPEED;
-            pf = p0 + v * t + 0.5 * a * t * t;
+        // How far we need to turn
+        let angle_diff = angle_diff(heading(), target_heading);
+
+        // How fast we are spinning
+        // > 0, we are spinning counter-clockwise
+        // < 0, we are spinning clockwise
+        let v = angular_velocity();
+
+        // How long it will takes us to kill our angular velocity
+        let seconds_to_stop = v.abs() / max_angular_acceleration();
+
+        // How long it will take us to reach the target heading giving the current v
+        let seconds_to_target_heading = angle_diff / v;
+
+        // We should start breaking if we would overshoot our target otherwise
+        let should_break = 
+            seconds_to_stop > 0.0 
+            && seconds_to_target_heading > 0.0
+            && seconds_to_stop >= seconds_to_target_heading;
+
+        // Info to help us debug
+        if self.debug_turn {
+            debug!("target: {}; heading: {};", radian_to_degree(target_heading), radian_to_degree(heading()));
+            debug!("degrees to turn: {};", radian_to_degree(angle_diff));
+            debug!("v: {}; max a: {};", radian_to_degree(v), radian_to_degree(max_angular_acceleration()));
+            debug!("to stop: {}; to target: {}; break: {}", seconds_to_stop, seconds_to_target_heading, should_break);
         }
 
-        pf
-    }
-
-    // TODO: properly calcurlate how much torque to add to "perfectly" turn the ship
-    fn turn(&self, angle: f64) {
-        if angle.abs() > degree_to_radian(60.0) {
-            if angle < 0.0 {
-                torque(-1.0 * max_angular_acceleration())
+        if should_break {
+            // Break!
+            if v > 0.0 {
+                torque(-1.0 * max_angular_acceleration());
             } else {
-                torque(max_angular_acceleration())
+                torque(max_angular_acceleration());
             }
         } else {
-            turn(angle);
+            let a = max_angular_acceleration();
+            
+            // Using torque can be hard once we are close to our target
+            // As when the target changes we tend to overshoot back and forth
+            // Instead we will use `turn` for the last leg
+            if seconds_to_target_heading < self.seconds_before_using_turn {
+                turn(target_heading - heading());
+            }
+
+            // Spin!
+            if angle_diff > 0.0 {
+                torque(a);
+            } else {
+                torque(-1.0 * a);
+            }
         }
     }
-    
+
     pub fn tick(&mut self) {
         self.scan();
 
-        if let Some(t) = self.closest_target.as_ref() {
-            draw_line(position(), t.position(), 0x00ff00);
-    
-            let target_in_time = self.calc_future_target(100);
-            let target_in_time_ang = angle_diff(
-                heading(), 
-                // target_in_time.angle()
-                (target_in_time - position()).angle()
-            );
+        if self.scan_result.is_some() {
+            let p1 = self.calculate_p1();
+            
+            // What our heading needs to be to face p1
+            let target_heading = (p1 - position()).angle();
+            
+            // how far we need to turn
+            let angle_to_turn = angle_diff(heading(), target_heading);
 
-            draw_line(position(), target_in_time, 0x47cbe6);
-    
-            if target_in_time_ang.abs() < degree_to_radian(5.0) {
-                fire(0);
+            // draws a green line from our ship to the target ship
+            // this is useful to visualize what is happening
+            draw_line(position(), target(), 0x00ff00);
+
+            // draws a cyan line to p1 of the target
+            // this is where we should be aiming
+            draw_line(position(), p1, 0x47cbe6);
+
+            // Only fire if we are facing p1
+            // Note: everything is in floats, so p1_angle will never be exactly 0
+            if angle_to_turn.abs() < self.firing_offset {
+                fire(0); // this tell the ship to fire weapon number '0'
             }
 
-            // TODO: refine my improved turn to actually be better
-            //self.turn(target_in_time_ang);
-            turn(target_in_time_ang * 1000.0);
-
-            let a = vec2(0.1 * max_forward_acceleration(), 0.0).rotate((target_in_time - position()).angle());
-            accelerate(a);
-        } else {
-            let a = velocity() * max_forward_acceleration() * -1.0;
-            accelerate(a);
+            // Turn to face the target
+            self.turn(target_heading);
         }
     }
 }
+
